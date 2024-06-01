59b985ac2901d8693faeb87996dfa8dee37efe93
SanchithaSamarasinghe <139639020+SanchithaSamarasinghe@users.noreply.github.com>
2024-06-01 13:01:22 +0530

    add controls key bindings

--- snake.py
+++ snake.py
@@ -5,7 +5,7 @@ delay = 0.1
 #setup screen
 wn = turtle.Screen()
 wn.title("Snakebites")
-wn.bgcolor("dark blue")
+wn.bgcolor("black")
 wn.setup(width=600, height=600)
 wn.tracer(0)
 
@@ -13,22 +13,55 @@ wn.tracer(0)
 head = turtle.Turtle()
 head.speed(0)
 head.shape("circle")
-head.color("black")
+head.color("red")
 head.penup()
 head.goto(0,0)
-head.direction = "up"
+head.direction = "stop"
 
 #function
+
+def go_up():
+    head.direction = "up"
+
+def go_down():
+    head.direction = "down"
+
+def go_right():
+    head.direction = "right"
+
+def go_left():
+    head.direction = "left"
+
 def move():
     if head.direction == "up":
         y = head.ycor()
         head.sety(y+20)
+
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y-20)
+
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x+20)
+
+    if head.direction == "left" :
+        x = head.xcor()
+        head.setx(x-20)         
+
+#controls
+
+wn.listen()
+wn.onkeypress(go_up, "w")
+wn.onkeypress(go_down, "s")
+wn.onkeypress(go_left, "a")
+wn.onkeypress(go_right, "d")
+
 
 #Main game loop
 while True:
     wn.update()
     move()
     time.sleep(delay)
-
-
+    
 wn.mainloop()
